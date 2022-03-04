fc3cefb49460251d050a3eebf959c5c56fc6dc1a
Ethan Havemann <94627102+echavemann@users.noreply.github.com>
2022-03-03 18:31:41 -0600

    mostly works

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer')
 
-const netID: ''       //fill this in
-const password: ''    //fill this in
+const netID = 'netid'
+const password=  'pword'    //fill this in
 
-const subject = 'COMP_SCI'
-const courseNUM = '214'
+const subject = 'subject'
+const courseNUM = 'num'
+
+
 
 async function launch() {
     const browser = await puppeteer.launch({headless: false})
@@ -27,16 +29,33 @@ async function launch() {
     const chooseCourse = await page.waitForXPath('/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div[3]/section/div/div[3]/div/div[1]/div[2]/div/div[2]/div/div/div/div[8]/div[1]/div');
     await chooseCourse.click();
 
+
     await page.waitForNavigation();
 
     const chooseClass = await page.waitForXPath('/html/body/form/div[2]/div[4]/div[1]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li[3]/div[2]/div');
     await page.waitForTimeout(500);
     chooseClass.click();
-    await page.waitForTimeout(500);
+    await page.waitForTimeout(1000);
+    
+    //why arent we using splinter
+    //await page.select("#SSR_CLSRCH_WRK_SUBJECT_SRCH$0","COMP_ENG");
+    await page.mouse.click(600,385);
+     let subjecta = subject.split("")
+        for (var i = 0; i< subjecta.length; i++) {
+        await page.keyboard.press(subjecta.at(i));
+    } 
+    await page.keyboard.press('Enter');
 
-    //A bug here - can't seem to find that element for some reason to select for a subject
-    const selectElem = await page.$('select[name="SSR_CLSRCH_WRK_SUBJECT_SRCH$0"]');
-    await selectElem.type("COMP_SCI");
-}
+    await page.mouse.click(850,400);
+    let pgnum = courseNUM.split("");
+        for (var i = 0; i<pgnum.length; i ++) {
+            await page.keyboard.press(pgnum.at(i));
+        }
+
+
+    await page.mouse.click(600,450);
+
+    await page.keyboard.press('Enter');
+}   
 
 launch()
