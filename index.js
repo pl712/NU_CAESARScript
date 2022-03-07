5b4eab40090595fa71294527b4d89cb625e1153e
pl712 <85031672+pl712@users.noreply.github.com>
2022-03-07 16:31:45 -0600

    v1

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const puppeteer = require('puppeteer')
 
-const netID = 'username'
-const password=  'password'    //fill this in
+const netID = 'plm7912'
+const password=  ''    //fill this in
 
-const subject = 'ELEC_ENG'
-const courseNUM = '327'
+const subject = 'COMP_SCI'
+const courseNUM = '212'
 
-//const browser = await puppeteer.launch({headless: false})
-
-// const browser = await puppeteer.launch({
-// args: [
-//     '--window-size=1920,1080'
-// ],
-// });
 async function launch() {
-    const browser = await puppeteer.launch({
-        headless:false,
-        args: [
-            '--window-size=1920,1080',],
-        });
+    const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
     await page.setViewport( { 'width' : 1600, 'height' : 1024 } );
     const addr = "https://caesar.ent.northwestern.edu/"
@@ -37,50 +26,26 @@ async function launch() {
 
     const chooseCourse = await page.waitForXPath('/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div[3]/section/div/div[3]/div/div[1]/div[2]/div/div[2]/div/div/div/div[8]/div[1]/div');
     await chooseCourse.click();
-
-
-    await page.waitForNavigation();
+    await page.waitForTimeout(500);
 
     const chooseClass = await page.waitForXPath('/html/body/form/div[2]/div[4]/div[1]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li[3]/div[2]/div');
     await page.waitForTimeout(500);
-    chooseClass.click();
-    await page.waitForTimeout(1000);
+    await chooseClass.click();
     
-    //Loop inserts here
-    await page.mouse.click(600,385);
-     let subjecta = subject.split("")
-        for (var i = 0; i< subjecta.length; i++) {
-        await page.keyboard.press(subjecta.at(i));
-    } 
-    await page.keyboard.press('Enter');
+    const frameHandle = await page.$("iframe[id='main_target_win0']");
+    const frame = await frameHandle.contentFrame();
 
-    await page.mouse.click(850,400);
-    let pgnum = courseNUM.split("");
-        for (var i = 0; i<pgnum.length; i ++) {
-            await page.keyboard.press(pgnum.at(i));
-        }
+    const subjectSelector = await frame.waitForXPath('/html/body/form/div[5]/table/tbody/tr/td/div/table/tbody/tr[4]/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/select')
+    await subjectSelector.select(subject);
 
+    const courseNum = await frame.waitForXPath('/html/body/form/div[5]/table/tbody/tr/td/div/table/tbody/tr[4]/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[4]/td[4]/div/input')
+    await courseNum.type(courseNUM);
 
-    await page.mouse.click(600,450);
+    const existCheck = await frame.waitForXPath('/html/body/form/div[5]/table/tbody/tr/td/div/table/tbody/tr[4]/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[8]/td[3]/div/input[2]')
+    await existCheck.click();
 
-    await page.keyboard.press('Enter');
+    const searchBtn = await frame.waitForXPath('/html/body/form/div[5]/table/tbody/tr/td/div/table/tbody/tr[4]/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr[5]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[3]/div/a/span/input')
+    await searchBtn.click();
 }   
 
 launch()
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-var monitors = [];
-function loop(monitors) {
-    var length = monitors.length
-    //launch
-    while (true) {
-        for (var j = 0; j<length; j++) {
-            //navigate
-            //check class status
-        } 
-        sleep(120-length);
-    }
-}
