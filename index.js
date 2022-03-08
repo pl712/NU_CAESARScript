2840156613a069d719f8026ec4e5967c19cc43e3
pl712 <85031672+pl712@users.noreply.github.com>
2022-03-08 15:03:46 -0600

    v2
    Now we just need to parse the table:)

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const puppeteer = require('puppeteer')
 const netID = 'daddyfrank'
 const password=  ''    //fill this in
 
-const subject = 'COMP_SCI'
-const courseNUM = '212'
-
-var page
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function launch() {
-    
+var List = [
+    //format" {Subkect: "XXX", Number: "XXX"},
+    {Subject: "COMP_SCI", Number: "214"},
+    {Subject: "COMP_SCI", Number: "212"}
+];
+//put the list of class you'd like to search here, in the given format
+
+async function page(classList) {
     const browser = await puppeteer.launch({headless: false})
     page = await browser.newPage()
+    
     await page.setViewport( { 'width' : 1600, 'height' : 1024 } );
     
     await page.goto("https://caesar.ent.northwestern.edu/")
@@ -35,16 +37,25 @@ async function launch() {
     await chooseCourse.click();
     await page.waitForTimeout(1000);
 
+    while (true) {
+        for (var j = 0; j<classList.length; j++) {
+            await cycle(classList[j].Subject, classList[j].Number, page);
+            await page.reload();
+            ///if it sees something, break
+        } 
+        console.log("\n");
+        await sleep(10000);
+    }
 }   
 
-async function cycle(classSubj, classNum) {
-    await page.reload();
+async function cycle(classSubj, classNum, page) {
 
     const chooseClass = await page.waitForXPath('/html/body/form/div[2]/div[4]/div[1]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li[3]/div[2]/div');
     await page.waitForTimeout(1000);
     await chooseClass.click();
     
     const frameHandle = await page.$("iframe[id='main_target_win0']");
+    await page.waitForTimeout(300);
     const frame = await frameHandle.contentFrame();
 
     const subjectSelector = await frame.waitForXPath('/html/body/form/div[5]/table/tbody/tr/td/div/table/tbody/tr[4]/td[2]/div/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[3]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/table/tbody/tr/td/table/tbody/tr[2]/td[2]/div/select')
@@ -63,29 +74,9 @@ async function cycle(classSubj, classNum) {
     const searchTable = await searchResult.contentFrame();
     
     const result = await searchTable.waitForXPath('/html/body/form/div[5]/table/tbody/tr/td/div/table/tbody/tr[12]/td[2]/div/table'); //The result table
+    await result.screenshot({path: "./screenshot.png"});
+    const d = new Date();
+    console.log("Searched for Course: " + classSubj + " " + classNum + " at time: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
 }
 
-var classList = [
-    {Subject: "COMP_SCI", Number: "214"}
-];
-
-function loop(monitors) {
-    var length = monitors.length
-
-    launch()
-    
-    while (true) {
-        for (var j = 0; j<length; j++) {
-            cycle(monitors[j].Subject, monitors[j].Number);
-            sleep(120-length);
-            //console.log("Looped")
-            //if (parsefindtru){
-            //    notify user
-            //    break loop
-            //}
-        } 
-        
-    }
-}
-
-loop(classList);
+page(List);
